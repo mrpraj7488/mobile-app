b242402ef7f7aa41a18f8ab2e2d4885d56d984ff
mrpraj7488 <mr.praj7488@gmail.com>
2025-09-04 22:11:35 +0530

    Updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,28 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
+    ignores: [
+      "dist/*",
+      "node_modules/*",
+      "android/*",
+      "ios/*",
+      ".expo/*",
+      "babel.config.js",
+      "metro.config.js",
+      "*.config.js"
+    ],
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+      "no-debugger": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      "object-shorthand": "error",
+      "prefer-template": "error",
+      "prefer-arrow-callback": "error"
+    }
   }
 ]);
